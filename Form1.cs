ed7fc1f92f3a230da9f037317e760fac40ccff4d
Paul Alfredo <155785978+paulafredo@users.noreply.github.com>
2025-04-13 16:33:15 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AobToByte
+{
+    public partial class AobToByte: Form
+    {
+        public AobToByte()
+        {
+            InitializeComponent();
+        }
+
+        private void AobToByte_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+
+
+
